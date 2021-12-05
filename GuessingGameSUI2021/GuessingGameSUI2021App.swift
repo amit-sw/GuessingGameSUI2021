fc102bc594171ae6cc9d55853bcff3df32a4cd6a
Amit Gupta <amit@gprof.com>
2021-12-05 07:47:06 -0800

    Initial Commit

---
+++ GuessingGameSUI2021/GuessingGameSUI2021App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameSUI2021App.swift
+//  GuessingGameSUI2021
+//
+//  Created by Amit Gupta on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameSUI2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
