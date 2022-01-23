e05bef0df8c5c85335f2d1caae039fb7d2169d8e
Amit Gupta <amit@gprof.com>
2022-01-23 12:56:49 -0800

    Initial commit

--- GuessingGameSUI2021/ContentView.swift
+++ GuessingGameSUI2021/ContentView.swift
@@ -7,10 +7,126 @@
 
 import SwiftUI
 
+var numberToGuess=0
+var lowRange=0
+var highRange=100
+var totalGuesses=10
+var guessesUsed=0
+
 struct ContentView: View {
+    @State var gameName="Guessing Game"
+    @State var topText=""
+    @State var guessCountLabel=""
+    @State var nextGuess=""
+    @State var butttonLabel="Submit"
+    @State var gameOn=true
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack (alignment: .center) {
+            Text(gameName)
+                .font(.system(size: 40))
+            Spacer()
+            Text(topText)
+                .font(.system(size: 30))
+            Text(guessCountLabel)
+                .font(.system(size: 20))
+            Spacer()
+            TextField("Next guess",text:$nextGuess)
+                .font(.system(size: 30))
+                .multilineTextAlignment(.center)
+            Button(butttonLabel) {
+                if(gameOn) {
+                if let l = Int(nextGuess) {
+                    processGuess(l)
+                }
+                } else {
+                    initializeGame()
+                }
+            }
+            .font(.system(size:40))
             .padding()
+            .background(.yellow)
+            .clipShape(Capsule())
+            Spacer()
+        }
+        .onAppear(perform: initializeGame)
+        .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 0, idealHeight: .infinity, maxHeight: .infinity)
+        .font(.system(size: 40))
+        
+    }
+    
+    
+    
+    /**
+     * Core Swift code for processing user input
+     */
+    
+    func generateRandomNumber() {
+        numberToGuess = Int.random(in: lowRange ... highRange)
+        print("Generated random number:",numberToGuess)
+    }
+    
+    func initializeGame() {
+        generateRandomNumber()
+        lowRange=0
+        highRange=100
+        totalGuesses=10
+        guessesUsed=0
+        topText="Guess x\n \(lowRange) < x < \(highRange)"
+        guessCountLabel=""
+        nextGuess=""
+        butttonLabel="Submit"
+        gameOn=true
+    }
+    
+    func processGuess(_ guessedNumber: Int) {
+        guessesUsed += 1
+        print("Just saw new guess:",guessedNumber," vs. numberToGuess=",numberToGuess)
+        if(guessedNumber == numberToGuess) {
+            processMatch()
+            return
+        } else {
+            if(guessesUsed>=totalGuesses) {
+                processNoMatch()
+            }
+            if(guessedNumber<numberToGuess) { // Too low
+                if(guessedNumber>lowRange) {
+                    lowRange=guessedNumber
+                }
+            } else { // Too high
+                if(guessedNumber<highRange) {
+                    highRange=guessedNumber
+                }
+            }
+            updateGuessInformation()
+        }
+        return
+    }
+    
+    func processMatch() {
+        print("MATCHED!!")
+        gameOn=false
+         let successMsg=String(format:"You correctly guessed %d in %d tries",numberToGuess,guessesUsed)
+         topText=successMsg
+         nextGuess=""
+         guessCountLabel=""
+        butttonLabel="Well done!!"
+    }
+    
+    func processNoMatch() {
+        print("NOT MATCHED!!")
+        gameOn=false
+         let successMsg=String(format:"Sorry - the number was %d",numberToGuess)
+         topText=successMsg
+         nextGuess=""
+         guessCountLabel=""
+         butttonLabel="Better luck next time!!"
+    }
+    
+    func updateGuessInformation() {
+         guessCountLabel=String(format:"Tries remaining: %d",totalGuesses-guessesUsed)
+         topText=String(format:"What is X? \n \(lowRange) < x < \(highRange)")
+        nextGuess=""
     }
 }
 
